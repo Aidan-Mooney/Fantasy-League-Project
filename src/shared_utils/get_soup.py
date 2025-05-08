b35696ce20cb5c19ed165496559fa99798a855e6
Aidan-Mooney <aidanmooney2001@gmail.com>
2025-05-08 18:17:55 +0100

    edit test_get_soup and add an extra test

--- src/shared_utils/get_soup.py
+++ src/shared_utils/get_soup.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_soup(url):
+def get_soup(url: str) -> BeautifulSoup:
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, "html.parser")
