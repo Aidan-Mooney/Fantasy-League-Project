0280593f9c9e0e37c590ec7f820b429e1508c538
Aidan-Mooney <aidanmooney2001@gmail.com>
2025-05-09 14:50:41 +0100

    add mock s3 client fixture to conftest and add the neccessary requirements to dev tools

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,26 @@
-import pytest
+from pytest import fixture
+from os import environ
 from unittest.mock import patch, Mock
+from boto3 import client
+from moto import mock_aws
 
 
-@pytest.fixture
+@fixture(scope="function")
+def aws_credentials():
+    environ["AWS_ACCESS_KEY_ID"] = "test"
+    environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    environ["AWS_SECURITY_TOKEN"] = "test"
+    environ["AWS_SESSION_TOKEN"] = "test"
+    environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@fixture(scope="function")
+def s3_client(aws_credentials):
+    with mock_aws():
+        yield client("s3", region_name="eu-west-2")
+
+
+@fixture
 def mock_requests_get():
     with patch("src.shared_utils.get_soup.requests.get") as mock_get:
         mock_response = Mock()
