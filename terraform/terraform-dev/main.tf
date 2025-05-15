511e092cf935bd19beeb0f204936918bf17e07d1
Aidan-Mooney <aidanmooney2001@gmail.com>
2025-05-15 15:41:55 +0100

    test refactor of terraform structure

--- terraform/terraform-dev/main.tf
+++ terraform/terraform-dev/main.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source ="hashicorp/aws"
-      version="~> 5.0"
-    }
-  }
-  backend "s3" {
-    bucket         = "aid-moo-terraform-state"
-    key            = "Fantasy-League-Project/dev/terraform.tfstate"
-    region         = "eu-west-2"
-  }
-}
-
-provider "aws"{
-  region = "eu-west-2"
-  default_tags {
-    tags={
-      ProjectName   = "Fantasy League Project"
-      DeployedFrom  = "Terraform"
-      Repository    = "Fantasy-League-Project"
-      Environment   = "Dev"
-    }
-  }
+module "shared_bucket" {
+  source      = "../modules/shared"
 }
