729f2014384dedf7fb77cd7015081ac52e4f6796
Aidan-Mooney <aidanmooney2001@gmail.com>
2025-05-19 11:46:14 +0100

    change some prefixes to locals instead of variables

--- terraform/modules/shared/s3.tf
+++ terraform/modules/shared/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "code_bucket" {
-  bucket_prefix = var.code_bucket_prefix
+  bucket_prefix = local.code_bucket_prefix
   tags = {
     purpose = "holds python code for lambda files."
   }
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "code_bucket" {
 
 
 resource "aws_s3_bucket" "fbref_fixture_tracker" {
-  bucket_prefix = var.fbref_fixture_tracker_prefix
+  bucket_prefix = local.fbref_fixture_tracker_prefix
   tags = {
     step = "extract"
     purpose = "hold codes of matches that have been processed."
--- terraform/modules/shared/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "code_bucket_prefix" {
-  default = "code-bucket-"
-}
-
-
-variable "fbref_fixture_tracker_prefix" {
-  default = "fixture-processing-tracker-"
-}
