0d69fc05e16567ce4a4ca50c14603caae491272c
Aidan-Mooney <aidanmooney2001@gmail.com>
2025-06-10 16:20:37 +0100

    write terraform for archiving the source code and uploading to code bucket

--- terraform/modules/extract/locals.tf
+++ terraform/modules/extract/locals.tf
@@ -1,3 +1,4 @@
 locals {
   get_match_codes_name = "get_match_codes"
+  extract_match_name   = "extract_match"
 }
--- terraform/modules/extract/s3.tf
+++ terraform/modules/extract/s3.tf
@@ -13,3 +13,18 @@ resource "aws_s3_object" "get_match_codes_file" {
   etag   = data.archive_file.get_match_codes.output_md5
 }
 
+
+data "archive_file" "extract_match" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_file      = "${var.project_directory}/src/extract/extract_fbref/extract_match.py"
+  output_path      = "${var.project_directory}/packages/extract_match.zip"
+}
+
+
+resource "aws_s3_object" "extract_match_file" {
+  bucket = var.code_bucket
+  key    = "extract/extract_fbref/extract_match.zip"
+  source = data.archive_file.extract_match.output_path
+  etag   = data.archive_file.extract_match.output_md5
+}
